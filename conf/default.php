bec7d1149767c86cf09a9e28b289873c72130c50
geraldraaf <gerald@raaf.name>
2023-02-02 08:55:47 +0100

    Update default.php

--- conf/default.php
+++ conf/default.php
@@ -6,6 +6,9 @@
  */
 
 $conf['http_header_name'] = 'HTTP_X_SSL_CLIENTCERT_BASE64';
+$conf['name_var'] = '2.16.840.1.113730.3.1.3';
+$conf['fullname_var'] = 'subject, CN';
+$conf['email_var'] = 'extensions, subjectAltName, email';
 $conf['group'] = 'smartcarduser';
 $conf['debug'] = 0;
 
